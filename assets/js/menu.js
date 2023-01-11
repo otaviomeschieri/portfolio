aa232029493202735f641866b016c79337aebf92
Otavio Meschieri <otaviomeschieri@gmail.com>
2023-01-10 21:57:23 -0300

    Ajustes no menu mobile

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,29 +6,45 @@ function handleButtonClick(event) {
     if (event.type === "touchstart") event.preventDefault();
     event.stopPropagation();
     nav.classList.toggle("active");
-    handleClickOutside(menu, () => {
-        nav.classList.remove("active");
-        // setAria();
-    });
+    // handleClickOutside(menu, () => {
+    //     nav.classList.remove("active");
+    //     // setAria();
+    // });
+
+    menuMobile();
     // setAria();
 }
 
-function handleClickOutside(targetElement, callback) {
-    const html = document.documentElement;
-    function handleHTMLClick(event) {
-        if (!targetElement.contains(event.target)) {
-            targetElement.removeAttribute("data-target");
-            html.removeEventListener("click", handleHTMLClick);
-            html.removeEventListener("touchstart", handleHTMLClick);
-            callback();
-        }
+function menuMobile() {
+    let icone = document.querySelector(".material-symbols-outlined");
+    let trocaTexto = document.querySelector(".menu__botao");
+    
+    if (icone.innerHTML === "menu") {
+        // trocaTexto.innerHTML = "Fechar menu";
+        icone.innerHTML = "close";
+    } else {
+        // trocaTexto.innerHTML = "Abrir menu";
+        icone.innerHTML = "menu"
     }
-    if (!targetElement.hasAttribute("data-target")) {
-        html.addEventListener("click", handleHTMLClick);
-        html.addEventListener("touchstart", handleHTMLClick);
-        targetElement.setAttribute("data-target", "");
-    }
+
 }
+
+// function handleClickOutside(targetElement, callback) {
+//     const html = document.documentElement;
+//     function handleHTMLClick(event) {
+//         if (!targetElement.contains(event.target)) {
+//             targetElement.removeAttribute("data-target");
+//             html.removeEventListener("click", handleHTMLClick);
+//             html.removeEventListener("touchstart", handleHTMLClick);
+//             callback();
+//         }
+//     }
+//     if (!targetElement.hasAttribute("data-target")) {
+//         html.addEventListener("click", handleHTMLClick);
+//         html.addEventListener("touchstart", handleHTMLClick);
+//         targetElement.setAttribute("data-target", "");
+//     }
+// }
 
 // function setAria() {
 //     const isActive = nav.classList.contains("active");
@@ -45,5 +61,4 @@ btnMenu.addEventListener("touchstart", handleButtonClick);
 
 function locationreload() {
     location.reload();
-
 }
